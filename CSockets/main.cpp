17de3cb1e4c4097a38dca6e6d2a68788ba7cce46
RobertSan <robertsandru96@gmail.com>
2017-03-07 17:16:12 +0200

    Initial commit / included libraries

---
+++ CSockets/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  CSockets
+//
+//  Created by Robert Sandru on 3/7/17.
+//  Copyright © 2017 Robert Sandru. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
